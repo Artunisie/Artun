fbfac25ac34f3de74a1531eaf89cccabc5fe2bf3
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-10-12 22:19:43 +0200

    user api

---
+++ Dev/Web/src/user.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+let userShema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    ncin:{type:Number,required:true},
+    ntel:{type:Number,required:true},
+    adresse:{type:String,required:true}
+})
+
+userShema.plugin(mongoosePaginate);
+const User=mongoose.model("Book",userShema);
+export default User;
