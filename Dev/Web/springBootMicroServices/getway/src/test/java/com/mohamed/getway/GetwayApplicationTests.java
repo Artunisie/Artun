3d83e9819b4d8483c367a2a709a8f5ed891c53b8
Mohamed <ahnbi32@gmail.com>
2023-10-22 10:55:50 +0200

    adding spring boot microservice folder

---
+++ Dev/Web/springBootMicroServices/getway/src/test/java/com/mohamed/getway/GetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
