a09a5b473655eea530a2ba6a56a2ad82756c38e2
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-10-28 15:15:14 +0100

    fix bug

--- Dev/Web/nodeMicroServices/getaway-service/dist/app.js
+++ Dev/Web/nodeMicroServices/getaway-service/dist/app.js
@@ -6,21 +6,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use('/users', (req, res) => {
-    try {
+const routes = [
+    { path: '/user-service', target: 'http://localhost:3000' },
+];
+app.all('*', (req, res) => {
+    const { url, method } = req;
+    const route = routes.find(r => url.match(r.path));
+    if (route) {
+        const targetURL = url.replace(route.path, route.target);
         const options = {
-            method: req.method,
+            method,
             headers: req.headers,
         };
-        const proxyRequest = http_1.default.request(process.env.USER_SERVICE_BASE_URL + req.originalUrl, options, (proxyRes) => {
-            res.status(proxyRes.statusCode || 500);
-            proxyRes.pipe(res);
-        });
-        req.pipe(proxyRequest);
+        const handleProxyRequest = () => {
+            const proxyRequest = http_1.default.request(targetURL, Object.assign(Object.assign({}, options), { headers: { 'Content-Type': 'application/json' } }), (proxyRes) => {
+                res.status(proxyRes.statusCode || 500);
+                proxyRes.pipe(res);
+            });
+            proxyRequest.on('error', (error) => {
+                console.error('Error in proxy request:', error);
+                res.status(500).json({ error: 'Failed to process the request' });
+            });
+            req.pipe(proxyRequest);
+        };
+        if (method === 'GET' || method === 'POST' || method === 'PUT' || method === 'DELETE') {
+            handleProxyRequest();
+        }
+        else {
+            res.status(405).send('Method Not Allowed');
+        }
     }
-    catch (error) {
-        res.status(500).json({ error: 'Failed to process the request' });
+    else {
+        res.sendStatus(404).send('Route not found');
     }
 });
 const PORT = 8888;
--- Dev/Web/nodeMicroServices/getaway-service/src/app.ts
+++ Dev/Web/nodeMicroServices/getaway-service/src/app.ts
@@ -1,25 +1,45 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import http from 'http';
+import { Request, Response } from 'express';
 
 const app = express();
 
-app.use(express.json());
+const routes = [
+  { path: '/user-service', target: 'http://localhost:3000' },
+];
 
-app.use('/users', (req: Request, res: Response) => {
-  try {
+app.all('*', (req, res) => {
+  const { url, method } = req;
+  const route = routes.find(r => url.match(r.path));
+
+  if (route) {
+    const targetURL = url.replace(route.path, route.target);
     const options = {
-      method: req.method,
+      method,
       headers: req.headers,
     };
 
-    const proxyRequest = http.request(process.env.USER_SERVICE_BASE_URL + req.originalUrl, options, (proxyRes) => {
-      res.status(proxyRes.statusCode || 500);
-      proxyRes.pipe(res);
-    });
+    const handleProxyRequest = () => {
+      const proxyRequest = http.request(targetURL, { ...options, headers: { 'Content-Type': 'application/json' } }, (proxyRes) => {
+        res.status(proxyRes.statusCode || 500);
+        proxyRes.pipe(res);
+      });
 
-    req.pipe(proxyRequest);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to process the request' });
+      proxyRequest.on('error', (error) => {
+        console.error('Error in proxy request:', error);
+        res.status(500).json({ error: 'Failed to process the request' });
+      });
+
+      req.pipe(proxyRequest);
+    };
+
+    if (method === 'GET' || method === 'POST' || method === 'PUT' || method === 'DELETE') {
+      handleProxyRequest();
+    } else {
+      res.status(405).send('Method Not Allowed');
+    }
+  } else {
+    res.sendStatus(404).send('Route not found');
   }
 });
 
