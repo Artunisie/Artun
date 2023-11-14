0c1ebac5373e4807da18fc5106502c976994045f
Mohamed <ahnbi32@gmail.com>
2023-11-14 18:27:50 +0100

    first commit

---
+++ Dev/Web/backend/springBootMicroServices/historyService/src/main/java/com/Martun/historyService/enumeration/HistoryType.java
@@ -0,0 +1,13 @@
+package com.Martun.historyService.enumeration;
+
+public enum HistoryType {
+    ACCOUNT_CREATION,
+    PROFILE_UPDATE,
+    PROJECT_COMPLETION,
+    POST_JOB,
+    APPLY_FOR_JOB,
+    ACCEPT_JOB_APPLICATION,
+    COMPLETE_JOB,
+    RATE_TECHNICIAN,
+    PAYMENT_RECEIVED,
+}
